fa650b2b29733f8e0031a89d154ce1a879c83e53
Rickson Rocha <ricksonroccha21@gmail.com>
2024-12-23 10:23:38 -0300

    [feat]: add EnableFeignClients annotation

--- src/main/java/com/br/rncar/project/ProjectApplication.java
+++ src/main/java/com/br/rncar/project/ProjectApplication.java
@@ -1,13 +1,23 @@
 package com.br.rncar.project;
 
+import com.br.rncar.project.service.CepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProjectApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+
 	}
+
+
 
 }
